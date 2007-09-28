be4ca6de23391e03b93462561079adf832e2a70a
knutert <knutert@9b39e7cb-2d3b-0410-a2e3-0fe6853e8d6e>
2007-09-28 08:40:27 +0000

    Added function for exact solution, plus some more comments

--- trunk/f.m
+++ trunk/f.m
@@ -1,5 +1,6 @@
 function l = f(x)
 % Calculates the load at a given x-coordinate
-% l=1; % load for -uxx=1
+
+l=1; % load for -uxx=1
 % l=x+1; % load for -uxx=x+1
-l=x*x; % load for -uxx=x^2
+% l=x*x; % load for -uxx=x^2
